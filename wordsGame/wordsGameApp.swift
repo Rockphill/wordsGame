1bbb028d6c92bbccb03a5183912c89328717c664
Rockphill <macwin33@gmail.com>
2022-12-12 11:48:09 +0600

    Initial Commit

---
+++ wordsGame/wordsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordsGameApp.swift
+//  wordsGame
+//
+//  Created by Andrei Sergienko on 12.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct wordsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
